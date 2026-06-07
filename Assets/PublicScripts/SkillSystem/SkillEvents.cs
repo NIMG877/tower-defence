9a473254cc222260d3ee0e9e752b19ac7996e638
NIMG877 <2956622854@qq.com>
2026-06-07 13:19:59 +0800

    feat(skill-system): concrete SkillEvent subclasses

---
+++ Assets/PublicScripts/SkillSystem/SkillEvents.cs
@@ -0,0 +1,95 @@
+namespace SkillSystem
+{
+    public class PreWarmEvent : SkillEvent { }
+    public class InitializeEvent : SkillEvent { }
+    public class BeforeAttackEvent : SkillEvent
+    {
+        public Entity target;
+        public float multiplyer = 1f;
+        public float defPenetrate;
+        public float mgrPenetrate;
+        public float defPenetrate_value;
+        public float mgrPenetrate_value;
+        public int cumbo = 1;
+        public int damageType;
+        public int applyType;
+    }
+    public class AfterAttackEvent : SkillEvent
+    {
+        public Entity target;
+        public float multiplyer;
+        public float defPenetrate;
+        public float mgrPenetrate;
+        public float defPenetrate_value;
+        public float mgrPenetrate_value;
+        public int damageType;
+        public int applyType;
+        public bool isDeadly;
+    }
+    public class BeforeTakeDamageEvent : SkillEvent
+    {
+        public Entity target;
+        public float multiplyer = 1f;
+        public float defPenetrate;
+        public float mgrPenetrate;
+        public float defPenetrate_value;
+        public float mgrPenetrate_value;
+        public int damageType;
+        public int applyType;
+    }
+    public class AfterTakeDamageEvent : SkillEvent
+    {
+        public Entity target;
+        public float multiplyer;
+        public float defPenetrate;
+        public float mgrPenetrate;
+        public float defPenetrate_value;
+        public float mgrPenetrate_value;
+        public int damageType;
+        public int applyType;
+        public bool isDeadly;
+    }
+    public class AttackSuccessfullyEvent : SkillEvent { }
+    public class AttackInterruptEvent : SkillEvent { }
+    public class BeforeHurtEvent : SkillEvent
+    {
+        public Entity origin;
+        public float damage;
+        public float multiplyer;
+        public float defPenetrate;
+        public float mgrPenetrate;
+        public float defPenetrate_value;
+        public float mgrPenetrate_value;
+        public int damageType;
+        public int applyType;
+        public bool isDeadly;
+    }
+    public class AfterHurtEvent : SkillEvent
+    {
+        public Entity origin;
+        public float damage;
+        public float multiplyer;
+        public float defPenetrate;
+        public float mgrPenetrate;
+        public float defPenetrate_value;
+        public float mgrPenetrate_value;
+        public int damageType;
+        public int applyType;
+        public bool isDeadly;
+    }
+    public class AttackAnimBeginEvent : SkillEvent { }
+    public class BeforeDieAnimationEvent : SkillEvent { }
+    public class DeathEvent : SkillEvent { }
+    public class IntervalTickEvent : SkillEvent
+    {
+        public float dt;
+    }
+    public class SkillBeginEvent : SkillEvent
+    {
+        public SkillRuntime skill;
+    }
+    public class SkillEndEvent : SkillEvent
+    {
+        public SkillRuntime skill;
+    }
+}
