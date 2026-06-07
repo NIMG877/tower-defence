fcafe08bb0591bdec5ae6847a05e75d64f27de0a
NIMG877 <2956622854@qq.com>
2026-06-07 13:15:58 +0800

    feat(skill-system): ComponentFactory + RegisterComponent attribute

---
+++ Assets/PublicScripts/SkillSystem/ComponentFactory.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace SkillSystem
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+    public class RegisterComponentAttribute : Attribute
+    {
+        public string TypeName;
+        public RegisterComponentAttribute(string typeName) { TypeName = typeName; }
+    }
+
+    public static class ComponentFactory
+    {
+        private static readonly Dictionary<string, Func<ISkillComponent>> _registry =
+            new Dictionary<string, Func<ISkillComponent>>();
+
+        public static void Register(string typeName, Func<ISkillComponent> ctor)
+        {
+            if (string.IsNullOrEmpty(typeName))
+                throw new ArgumentException("typeName must not be empty", nameof(typeName));
+            if (ctor == null)
+                throw new ArgumentNullException(nameof(ctor));
+            _registry[typeName] = ctor;
+        }
+
+        public static ISkillComponent Create(string typeName)
+        {
+            if (string.IsNullOrEmpty(typeName)) return null;
+            return _registry.TryGetValue(typeName, out var ctor) ? ctor() : null;
+        }
+
+        public static bool IsRegistered(string typeName) =>
+            !string.IsNullOrEmpty(typeName) && _registry.ContainsKey(typeName);
+
+        public static IEnumerable<string> RegisteredTypes => _registry.Keys;
+
+        public static void Clear() => _registry.Clear();
+    }
+}
