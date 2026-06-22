6eae39207df788f69e1262f167a1f3373869a817
NIMG877 <2956622854@qq.com>
2026-06-22 12:59:34 +0800

    feat(level-editor): 自定义 Inspector 骨架 + 共享样式

---
+++ Assets/Editor/LevelEditor/LevelDataEditor.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// LevelData 的自定义 Inspector (UI Toolkit)。
+/// 触发条件: 在 Project 窗口选中 LevelData 资产。
+/// </summary>
+[CustomEditor(typeof(LevelData))]
+public class LevelDataEditor : Editor
+{
+    public override VisualElement CreateInspectorGUI()
+    {
+        var root = new VisualElement();
+        root.style.flexDirection = FlexDirection.Column;
+
+        // Header
+        var header = new VisualElement();
+        header.AddToClassList("level-editor-header");
+        var title = new Label(((LevelData)target).LevelName);
+        title.AddToClassList("level-editor-title");
+        header.Add(title);
+        root.Add(header);
+
+        // Body placeholder
+        var body = new VisualElement();
+        body.style.paddingLeft = 12;
+        body.style.paddingRight = 12;
+        body.style.paddingTop = 12;
+        body.style.paddingBottom = 12;
+        body.Add(new Label("(Sections will be added in subsequent tasks)"));
+        root.Add(body);
+
+        return root;
+    }
+}
