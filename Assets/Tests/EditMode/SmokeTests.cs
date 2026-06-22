b3e87ed7940372ae39a51be189f08f320c6ea2b1
NIMG877 <2956622854@qq.com>
2026-06-22 12:27:21 +0800

    test(level-editor): 搭 EditMode 测试脚手架

---
+++ Assets/Tests/EditMode/SmokeTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Tests.EditMode
+{
+    public class SmokeTests
+    {
+        [Test]
+        public void Always_Passes()
+        {
+            Assert.AreEqual(1 + 1, 2);
+        }
+    }
+}
