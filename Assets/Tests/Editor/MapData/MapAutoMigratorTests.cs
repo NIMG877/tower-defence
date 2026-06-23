fe6be7f0edcc59dcb0f9e4c556308bfc48edfe2f
NIMG877 <2956622854@qq.com>
2026-06-23 22:14:28 +0800

    feat(level-editor): MapAutoMigrator + escape-hatch menu item

---
+++ Assets/Tests/Editor/MapData/MapAutoMigratorTests.cs
@@ -0,0 +1,84 @@
+using NUnit.Framework;
+using UnityEngine;
+
+namespace MapData.Tests
+{
+    public class MapAutoMigratorTests
+    {
+        [Test]
+        public void ReadFromPrefab_returns_empty_for_null_prefab()
+        {
+            var entries = MapAutoMigrator.ReadFromPrefab(null);
+            Assert.IsNotNull(entries);
+            Assert.AreEqual(0, entries.Count);
+        }
+
+        [Test]
+        public void ReadFromPrefab_returns_empty_for_prefab_with_no_children()
+        {
+            var root = new GameObject("MapRoot");
+            try
+            {
+                var entries = MapAutoMigrator.ReadFromPrefab(root);
+                Assert.IsNotNull(entries);
+                Assert.AreEqual(0, entries.Count);
+            }
+            finally
+            {
+                Object.DestroyImmediate(root);
+            }
+        }
+
+        [Test]
+        public void ReadFromPrefab_skips_children_without_BlockData()
+        {
+            var root = new GameObject("MapRoot");
+            try
+            {
+                var c0 = new GameObject("c0");
+                c0.transform.SetParent(root.transform, false);
+                // no BlockData attached (and BlockData.cs is gone)
+                var entries = MapAutoMigrator.ReadFromPrefab(root);
+                Assert.AreEqual(0, entries.Count);
+            }
+            finally
+            {
+                Object.DestroyImmediate(root);
+            }
+        }
+
+        [Test]
+        public void MigrateLevelData_returns_false_when_already_migrated()
+        {
+            var ld = ScriptableObject.CreateInstance<LevelData>();
+            ld.MapData = new System.Collections.Generic.List<BlockDataEntry>
+            {
+                new BlockDataEntry { i = 0, j = 0 },
+            };
+            try
+            {
+                Assert.IsFalse(MapAutoMigrator.MigrateLevelData(ld));
+            }
+            finally
+            {
+                Object.DestroyImmediate(ld);
+            }
+        }
+
+        [Test]
+        public void MigrateLevelData_returns_false_when_no_prefab()
+        {
+            var ld = ScriptableObject.CreateInstance<LevelData>();
+            ld.MapPrefab = null;
+            ld.MapData = new System.Collections.Generic.List<BlockDataEntry>();
+            try
+            {
+                Assert.IsFalse(MapAutoMigrator.MigrateLevelData(ld));
+            }
+            finally
+            {
+                Object.DestroyImmediate(ld);
+            }
+        }
+    }
+}
